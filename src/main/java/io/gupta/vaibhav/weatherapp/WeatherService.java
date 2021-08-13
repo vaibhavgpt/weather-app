0196186c3348c2d5d9ef9b0bc9270261f76f3b58
Vaibhav <simply.vaibhavgupta@gmail.com>
2021-08-13 11:59:04 +0530

    First commit

---
+++ src/main/java/io/gupta/vaibhav/weatherapp/WeatherService.java
@@ -0,0 +1,23 @@
+package io.gupta.vaibhav.weatherapp;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "unknown")
+    public String getWeather(){
+        return restTemplate
+                .getForEntity("http://weather-service/weather", String.class)
+                .getBody();
+    }
+
+    public String unknown(){
+        return "unknown";
+    }
+}
